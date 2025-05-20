23732415fc5d5f760e6ec5f1d88709c85ecf0b4b
lemongza <92498054+lemongza@users.noreply.github.com>
2025-05-20 09:51:49 +0900

    webpack config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'main.bundle.js'
-    },
-   module: {
-       rules: [
-         {
-           test: /\.js$/,
-           exclude: /node_modules/,
-           use: {
-             loader: 'babel-loader',
-             options: {
-               presets: ['@babel/preset-env']
-             }
-           }
-         }
-       ]
-     },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map',
-    mode: 'development'
+  entry: { app: "./js/main.js", ratefinder: "./js/ratefinder.js" },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js", //[] 안에 변수 동적으로 변환 위의 entry name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: "source-map",
+  mode: "development",
 };
